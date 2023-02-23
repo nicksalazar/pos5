548adac68ae8219bb195dfe542f4b30fdeecb35b
CFarias95 <andres95cfc@gmail.com>
2023-02-23 16:48:55 -0500

    cambio secuencial

---
+++ database/migrations/tenant/2023_02_22_173810_tenant_alter2_secuencial_to_retenciones_join.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Grammars\ChangeColumn;
+
+class TenantAlter2SecuencialToRetencionesJoin extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('retenciones_join', function (Blueprint $table) {
+
+            Schema::dropIfExists('secuencial');
+
+            $table->string('secuencial',20)->nullable();
+            
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('retenciones_join', function (Blueprint $table) {
+            //
+            Schema::dropIfExists('secuencial');
+        });
+    }
+}
